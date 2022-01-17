73ec4d5acbac464f3e4230b862f1b253e284aac1
ekinohito <zhenyagur9@gmail.com>
2022-01-17 05:26:06 +0300

    complete peace

--- test/controllers/chunks_controller_test.rb
+++ test/controllers/chunks_controller_test.rb
@@ -6,30 +6,15 @@ class ChunksControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "error without query" do
-    get chunks_index_url, as: :json
+  test "should get xml without ssr" do
+    get "#{chunks_index_url}?q=1,2,3&ssr=false", as: :xml
     assert_response :success
-    assert_equal 'application/json', @response.media_type
-    assert_includes @response.body, 'error'
+    assert_equal 'application/xml', @response.media_type
   end
 
-  test "consumes query" do
-    get "#{chunks_index_url}?q=1,2,3", as: :json
+  test "should get html with ssr" do
+    get "#{chunks_index_url}?q=1,2,3&ssr=true", as: :xml
     assert_response :success
-    assert_equal 'application/json', @response.media_type
-    data = JSON.parse(@response.body)
-    assert_equal [1, 2, 3], data['input']
-    assert_equal [[1, 2, 3]], data['chunks']
-    assert_equal [1, 2, 3], data['max_length']
-  end
-
-  test "complex_query" do
-    get "#{chunks_index_url}?q=3,4,1,2,3,2", as: :json
-    assert_response :success
-    assert_equal 'application/json', @response.media_type
-    data = JSON.parse(@response.body)
-    assert_equal [3, 4, 1, 2, 3, 2], data['input']
-    assert_equal [[3, 4], [1, 2, 3], [2]], data['chunks']
-    assert_equal [1, 2, 3], data['max_length']
+    assert_equal 'text/html', @response.media_type
   end
 end
